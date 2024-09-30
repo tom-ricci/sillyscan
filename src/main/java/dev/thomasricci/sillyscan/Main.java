40c0607fe6216b777508bf0013677b53f5c3d5fc
Thomas Ricci <me@thomasricci.dev>
2024-09-30 04:32:18 -0400

    spoof version

--- src/main/java/dev/thomasricci/sillyscan/Main.java
+++ src/main/java/dev/thomasricci/sillyscan/Main.java
@@ -3,8 +3,10 @@ package dev.thomasricci.sillyscan;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
 import org.slf4j.Logger;
 
 @Plugin(id = "sillyscan", name = "sillyscan", version = BuildConstants.VERSION)
@@ -18,7 +20,23 @@ public class Main {
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        // something will go here later
+    }
 
+    /**
+     * Spoofs the version of the server to prevent matscan from fingerprinting it as Velocity and skipping it.
+     */
+    @Subscribe
+    public void onPing(ProxyPingEvent event) {
+        ServerPing.Version version = event.getPing().getVersion();
+        ServerPing ping = event
+                .getPing()
+                .asBuilder()
+                .version(new ServerPing.Version(
+                    version.getProtocol(),
+                    version.getName().replaceFirst("Velocity", "").trim()))
+                .build();
+        event.setPing(ping);
     }
 
 }
