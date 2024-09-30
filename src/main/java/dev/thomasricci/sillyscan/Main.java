0aa5beabad35f0850659759f6ca96f25cfd2a812
Thomas Ricci <me@thomasricci.dev>
2024-09-30 03:31:01 -0400

    Initial commit

---
+++ src/main/java/dev/thomasricci/sillyscan/Main.java
@@ -0,0 +1,24 @@
+package dev.thomasricci.sillyscan;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+@Plugin(id = "sillyscan", name = "sillyscan", version = BuildConstants.VERSION)
+public class Main {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private ProxyServer server;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+
+    }
+
+}
